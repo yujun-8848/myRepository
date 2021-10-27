337e3adc8ed81db34e0582d7ea9cc9cbbcec3244
yjun <1505411476@qq.com>
2021-10-27 20:26:27 +0800

    add 算法体系班代码

---
+++ src/main/java/class01_test/Code01_SelectionSort.java
@@ -0,0 +1,30 @@
+package class01_test;
+
+/**
+ * 选择排序
+ */
+public class Code01_SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+
+        //0...N-1 中选出最小值  下标进行交换
+        //1...N-1 中选出最小值  下标进行交换
+        //2...N-1 中选出最小值  下标进行交换
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i; j < arr.length; j++) {
+
+
+            }
+
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
